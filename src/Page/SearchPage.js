acc706cdcbf8583d73f864aa96718c32523173e2
kwooS <kwsong0725@gmail.com>
2021-05-15 21:28:20 +0900

    Added ListMapView

--- src/Page/SearchPage.js
+++ src/Page/SearchPage.js
@@ -3,12 +3,20 @@ import {Link, useHistory} from 'react-router-dom';
 import { useSelector } from 'react-redux'
 import MapContainer from '../Components/SearchMap';
 import SearchHeaderView from '../Components/SearchHeaderView';
+import ListMapView from '../Components/ListMapView';
 import './SearchPage.css'
 const SearchPage = () => {
 
   return <div>
     <SearchHeaderView />
-    <div id="leftBox"></div>
+    <div id="leftBox">
+      <div>
+        <ListMapView />
+      </div>
+      <div>
+        <ListMapView />
+      </div>
+    </div>
     <div id="rightBox">
       <MapContainer />
     </div>
