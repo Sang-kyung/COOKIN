b7d8e78ce2e20eb07505168653f2a57835b0475f
simonp6 <simonp1113@gmail.com>
2021-05-15 14:32:09 +0900

    Added search page link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link, Switch } from 'react-router-dom';
 
 import HomePage from './Page/HomePage';
 import MyPage from './Page/MyPage';
+import SearchPage from './Page/SearchPage';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 
       <Route path="/home" component={HomePage} />
       <Route path="/mypage" component={MyPage} />
+      <Route path="/search" compoment={SearchPage} />
     </div>
   );
 }
--- src/Components/SearchMap.js
+++ src/Components/SearchMap.js
@@ -3,7 +3,9 @@ import React from 'react';
 const SearchMap = () => {
   var this_div = 
   <div>
-  
+      <div id="map" style="width:500px;height:400px;">
+        <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=61beca755e319b4491ac77d1b1e12efe."></script>
+      </div>
   </div>
   return this_div
 }
