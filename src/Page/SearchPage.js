4e54f9eb353fbc909395137ae760979b9d4728a9
kwooS <kwsong0725@gmail.com>
2021-05-15 21:20:07 +0900

    Add ListMapView on SearchPage

--- src/Page/SearchPage.js
+++ src/Page/SearchPage.js
@@ -3,12 +3,29 @@ import {Link, useHistory} from 'react-router-dom';
 import { useSelector } from 'react-redux'
 import MapContainer from '../Components/SearchMap';
 import MainHeaderView from '../Components/MainHeaderView';
+import ListMapView from '../Components/ListMapView'
 import './SearchPage.css'
 const SearchPage = () => {
 
   return <div>
     <MainHeaderView />
-    <div id="leftBox"></div>
+    <div id="leftBox">
+      <div>
+        <ListMapView />
+      </div>
+      <div>
+        <ListMapView />
+      </div>
+      <div>
+        <ListMapView />
+      </div>
+      <div>
+        <ListMapView />
+      </div>
+      <div>
+        <ListMapView />
+      </div>
+    </div>
     <div id="rightBox">
       <MapContainer />
     </div>
