465b6548b1763203f27cd272cb68e4ae0e877b95
kwooS <kwsong0725@gmail.com>
2021-05-18 23:16:22 +0900

    update searchview for first city

--- src/Components/Views/SearchView.js
+++ src/Components/Views/SearchView.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './SearchView.css'
+import * as SearchMap from './SearchMap'
+import { setFirstCity} from '../../reducers/searchCity';
+import { useDispatch} from 'react-redux'
 
 const SearchView = () => {
+  const dispatch = useDispatch();
+  const clickfunction = () => {
+    var input = document.getElementById("input").value;
+    dispatch(setFirstCity(input));
+    SearchMap.searchMapKeyWord(input);
+    window.location.href="/search";
+  }
+
   var this_div = 
   <div id="mainContainer">
   <div className= 'inputWrapper' >
-    <input className='input' placeholder="Enter your locations!">
+    <input type="text" id='input' placeholder="Enter your locations!">
     </input>
   </div>
   <div className='buttonWrapper'>
-    <button id="buttonSearch" onClick={() => window.location.href="/search"}>Search</button>
+    <button id="buttonSearch" onClick={clickfunction}>Search</button>
     <button id="buttonRecommendation">Recommend Me</button>
   </div>
   </div>
