62d111b0bcf00cc51117f26d9bfeba23d82042d8
kwooS <kwsong0725@gmail.com>
2021-05-18 20:06:08 +0900

    Allowed left div scroll + add css for ListMapView

--- src/Components/Views/ListMapView.js
+++ src/Components/Views/ListMapView.js
@@ -1,13 +1,15 @@
 import React from  'react'
-
+import "./ListMapView.css";
 
 const ListMapView = () => {
     var this_div = 
     <div>
+        <h4 id="name">
+            Restaurant name
+        </h4>
         
-
     </div>
-    return this_div
+    return this_div;
 }
 
 export default ListMapView;
