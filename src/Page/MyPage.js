946190886a034489ec0750f58c628bda8245a5cd
jinyeop <bb3598@naver.com>
2021-05-20 00:24:50 +0900

    mypage Ups, Pasts async solve

--- src/Page/MyPage.js
+++ src/Page/MyPage.js
@@ -10,13 +10,36 @@ import MyinformationView from '../Components/Views/MyinformationView';
 import MyreservationView from '../Components/Views/MyreservationView';
 import MainHeaderViewLeft from '../Components/Views/MainHeaderViewLeft';
 
+var Ups = new Array();
+var Pasts = new Array();
+
+const upsandpasts = (_res, present) => {
+  console.log('upsandpasts');
+  console.log(_res, present);
+  var date = new Date(_res.date);
+  date >= present ? (
+    Ups.push(_res)
+  ) : (
+    Pasts.push(_res)
+  )
+  return;
+}
+
+const UpsandPasts = (res) => {
+  console.log('UpsandPasts');
+  console.log(res);
+  const now = new Date();
+  res.map((item, index) => (upsandpasts(item, now))) 
+  return Ups, Pasts
+}
+
 const MyPage = () => {
   const history = useHistory();
   // const [phone, setPhone] = useState("");
   // const [name, setName] = useState("");
-
+  const res = useSelector(state => state.reservation.reservations);
   const dispatch = useDispatch();
-  const isloggedIn = useSelector(state => state.user.isloggedIn);
+  // const isloggedIn = useSelector(state => state.user.isloggedIn);
 
   // const _login = () => {
   //   dispatch(login({phone, name}));
@@ -37,20 +60,7 @@ const MyPage = () => {
   //   setName(e.target.value);
   // }
 
-  const res = useSelector(state => state.reservation.reservations);
-  const now = new Date();
-
-  let Ups = new Array();
-  let Pasts = new Array();
-  res.map(item => {
-    var date = new Date(item.date)
-    date >= now ? (
-      Ups.push(item)
-    ) : (
-      Pasts.push(item)
-    )
-  })
-
+  UpsandPasts(res);
   var Upcoming = Ups.length;
 
   // var condition = Ups.length > 0 && Pasts.length > 0;
