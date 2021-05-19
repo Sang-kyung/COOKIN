23b12906d98f7843efd977c478e845f9ae9e10cf
jinyeop <bb3598@naver.com>
2021-05-20 01:01:35 +0900

    enter mypage without login error solve

--- src/Page/MyPage.js
+++ src/Page/MyPage.js
@@ -1,8 +1,5 @@
-//import React, {useCallback} from 'react';
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import {login} from '../reducers/user';
-import {logout} from '../reducers/user';
 import React, { useState } from 'react';
 
 // view
@@ -29,51 +26,35 @@ const UpsandPasts = (res) => {
   console.log('UpsandPasts');
   console.log(res);
   const now = new Date();
-  res.map((item, index) => (upsandpasts(item, now))) 
+  res.map((item) => (upsandpasts(item, now)))
   return Ups, Pasts
 }
 
 const MyPage = () => {
   const history = useHistory();
-  // const [phone, setPhone] = useState("");
-  // const [name, setName] = useState("");
   const res = useSelector(state => state.reservation.reservations);
+  const isloggedIn = useSelector(state => state.user.isloggedIn);
   const dispatch = useDispatch();
-  // const isloggedIn = useSelector(state => state.user.isloggedIn);
-
-  // const _login = () => {
-  //   dispatch(login({phone, name}));
-  // }
-
-  // const _logout = () => {
-  //   dispatch(logout());
-  //   history.push("/");
-  // }
-
-  // const onPhoneChange = (e) => {
-  //   console.log(e.target.value);
-  //   setPhone(e.target.value);
-  // }
-
-  // const onNameChange = (e) => {
-  //   console.log(e.target.value);
-  //   setName(e.target.value);
-  // }
-
+  
   UpsandPasts(res);
   var Upcoming = Ups.length;
 
-  // var condition = Ups.length > 0 && Pasts.length > 0;
-
   return <div>
-          <MainHeaderViewLeft />
-          <MyinformationView Upcoming={Upcoming}/>
-          {/* {condition ? */}
-          <MyreservationView Ups={Ups} Pasts={Pasts}/>
-             {/* :
-             <div>"No reservation"</div>
-           } */}
-        </div>
+            {isloggedIn ? (
+              <div>
+                  <MainHeaderViewLeft />
+                  <MyinformationView Upcoming={Upcoming}/>
+                  {/* {condition ? */}
+                  <MyreservationView Ups={Ups} Pasts={Pasts}/>
+                  {/* :
+                  <div>"No reservation"</div>
+                } */}
+                </div>
+            ) : (
+              alert('You did not log in.') || history.push("/")
+            )
+            }
+          </div>
 }
 
 export default MyPage
