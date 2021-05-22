c2c77f576ec45b0c237a857706d7371a8987fe8c
simonp6 <simonp1113@gmail.com>
2021-05-22 13:35:45 +0900

    Update SearchTagItem.js

--- src/Components/Views/SearchTagItem.js
+++ src/Components/Views/SearchTagItem.js
@@ -4,8 +4,13 @@ import './SearchTagItem.css';
 // style
 
 const SearchTagItem = ({item}) => {
+  var visibilityOption = {display: ""};
+  if(item.city == '-'){
+    visibilityOption = {display: "None"};
+  }
 
-  return <div className="tagContainer">
+
+  return <div className="tagContainer" style={visibilityOption}>
         <div className="tagText">{item.city} </div>
         <button className="tagBtn" onClick = {item.func}>X</button>
       </div>
