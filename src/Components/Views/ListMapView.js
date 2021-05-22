851573481a89588a3d5cd846ab8e58b39d125b86
kwooS <kwsong0725@gmail.com>
2021-05-23 00:37:14 +0900

    Minor Fixes

--- src/Components/Views/ListMapView.js
+++ src/Components/Views/ListMapView.js
@@ -16,7 +16,7 @@ const ListMapView = ({kitchen}) => {
 
     var this_div = <div id="body">
                         <div id="photo">
-                            {kitchen && <img class="photo" src={require(`../../img/Kitchen/${kitchen.img[0]}.png`).default}/>}
+                            {kitchen && <img className="photo" src={require(`../../img/Kitchen/${kitchen.img[0]}.png`).default}/>}
                         </div>
                         <div id="content">
                             <div id="name" onClick={(e)=>{redirectfunction()}}>
