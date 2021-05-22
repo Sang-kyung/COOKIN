a1ea58c694228dc9da57da04f06702e4fd62cb20
jinyeop <bb3598@naver.com>
2021-05-22 22:55:45 +0900

    question comment

---
+++ src/Components/Views/QuestionView.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import queryString from 'query-string';
+
+// // view
+// // import DetailView from '../Components/Views/DetailView';
+
+// const QuestionView = () => {
+//     return <div>
+//         <button>
+//             <img className="icon" src={require(`../../img/Main/chef.png`).default} />
+//         </button>
+//     </div>
+// }
+
+// export default QuestionView
