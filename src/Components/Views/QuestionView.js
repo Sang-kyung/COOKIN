1a0e826e4a89443329f5de538cf3922bca77bed6
kwooS <kwsong0725@gmail.com>
2021-05-24 22:28:55 +0900

    QuestionView Step add

--- src/Components/Views/QuestionView.js
+++ src/Components/Views/QuestionView.js
@@ -32,7 +32,7 @@ const QuestionView = (props) => {
                     <Swiper pagination={{"type": "progressbar"}} navigation={true} className="mySwiper">
                         <SwiperSlide>
                                 <img className="pictures" src={require(`../../img/Tutorials/step1.png`).default} />
-                                <div className="div-descriptions"><span className="descriptions">Step 1. Type the one of your location and enter.</span></div>
+                                <div className="div-descriptions"><span className="descriptions">Step 1. Type one of your location and enter.</span></div>
                             </SwiperSlide>
                         <SwiperSlide>
                                 <img className="pictures" src={require(`../../img/Tutorials/step2.png`).default} />
@@ -40,19 +40,23 @@ const QuestionView = (props) => {
                             </SwiperSlide>
                         <SwiperSlide>
                                 <img className="pictures" src={require(`../../img/Tutorials/step3.png`).default} />
-                                <div className="div-descriptions"><span className="descriptions">Step 3. Click 'fetch' button to recommend your shortest location to cookout</span></div>
+                                <div className="div-descriptions"><span className="descriptions">Step 3. Click the 'Recommend' button to get your recommended shortest location for your cookout</span></div>
                             </SwiperSlide>
                         <SwiperSlide>
                                 <img className="pictures" src={require(`../../img/Tutorials/step4.png`).default} />
-                                <div className="div-descriptions"><span className="descriptions">Step 4. Click certain kitchen to look for specifications.</span></div>
+                                <div className="div-descriptions"><span className="descriptions">Step 4. Scroll to see kitchen choices and to look for kitchen details.</span></div>
                             </SwiperSlide>
                         <SwiperSlide>
                                 <img className="pictures" src={require(`../../img/Tutorials/step5.png`).default} />
-                                <div className="div-descriptions"><span className="descriptions">Step 5. Check the options and make a reservation.</span></div>
+                                <div className="div-descriptions"><span className="descriptions">Step 5. If you want to buy ingredients, choose the amount you want to buy through + and - signs.</span></div>
                             </SwiperSlide>
                         <SwiperSlide>
                                 <img className="pictures" src={require(`../../img/Tutorials/step6.png`).default} />
-                                <div className="div-descriptions"><span className="descriptions">Step 6. In My Page, check or cancel your reservation information.</span></div>
+                                <div className="div-descriptions"><span className="descriptions">Step 6. Choose your prefered date and time and reserve.</span></div>
+                            </SwiperSlide>    
+                        <SwiperSlide>
+                                <img className="pictures" src={require(`../../img/Tutorials/step6.png`).default} />
+                                <div className="div-descriptions"><span className="descriptions">Step 7. In My Page, check or cancel your reservation information.</span></div>
                             </SwiperSlide>
                         </Swiper>
                     {/* <button className="Qmodal-signupbtn" onClick={_signup}>
