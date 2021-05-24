18120921127d579630671f5285a6f4245b881e85
jinyeop <bb3598@naver.com>
2021-05-24 21:41:32 +0900

    question modal modified

--- src/Components/Views/QuestionView.js
+++ src/Components/Views/QuestionView.js
@@ -30,29 +30,33 @@ const QuestionView = (props) => {
                         How to use COOKIN
                     </div>
                     <Swiper pagination={{"type": "progressbar"}} navigation={true} className="mySwiper">
-                        <SwiperSlide>
+                    <SwiperSlide>
                                 <img className="pictures" src={require(`../../img/Tutorials/step1.png`).default} />
-                                <div className="div-descriptions"><span className="descriptions">Step 1. Type the one of your location and enter.</span></div>
+                                <div className="div-descriptions"><span className="descriptions">Step 1. Press the My Page button to sign up or log in.</span></div>
                             </SwiperSlide>
                         <SwiperSlide>
                                 <img className="pictures" src={require(`../../img/Tutorials/step2.png`).default} />
-                                <div className="div-descriptions"><span className="descriptions">Step 2. Keep registering your locations one by one.</span></div>
+                                <div className="div-descriptions"><span className="descriptions">Step 2. Type the one of your location and enter.</span></div>
                             </SwiperSlide>
                         <SwiperSlide>
                                 <img className="pictures" src={require(`../../img/Tutorials/step3.png`).default} />
-                                <div className="div-descriptions"><span className="descriptions">Step 3. Click 'fetch' button to recommend your shortest location to cookout</span></div>
+                                <div className="div-descriptions"><span className="descriptions">Step 3. Keep registering your locations one by one.</span></div>
                             </SwiperSlide>
                         <SwiperSlide>
                                 <img className="pictures" src={require(`../../img/Tutorials/step4.png`).default} />
-                                <div className="div-descriptions"><span className="descriptions">Step 4. Click certain kitchen to look for specifications.</span></div>
+                                <div className="div-descriptions"><span className="descriptions">Step 4. Click 'fetch' button to recommend your shortest location to cookout</span></div>
                             </SwiperSlide>
                         <SwiperSlide>
                                 <img className="pictures" src={require(`../../img/Tutorials/step5.png`).default} />
-                                <div className="div-descriptions"><span className="descriptions">Step 5. Check the options and make a reservation.</span></div>
+                                <div className="div-descriptions"><span className="descriptions">Step 5. Click certain kitchen to look for specifications.</span></div>
                             </SwiperSlide>
                         <SwiperSlide>
                                 <img className="pictures" src={require(`../../img/Tutorials/step6.png`).default} />
-                                <div className="div-descriptions"><span className="descriptions">Step 6. In My Page, check or cancel your reservation information.</span></div>
+                                <div className="div-descriptions"><span className="descriptions">Step 6. Check the options and make a reservation.</span></div>
+                            </SwiperSlide>
+                        <SwiperSlide>
+                                <img className="pictures" src={require(`../../img/Tutorials/step7.png`).default} />
+                                <div className="div-descriptions"><span className="descriptions">Step 7. In My Page, check or cancel your reservation information.</span></div>
                             </SwiperSlide>
                         </Swiper>
                     {/* <button className="Qmodal-signupbtn" onClick={_signup}>
