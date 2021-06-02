f8c2a21d900f478c9d5ec9acb45ee5d0cf42e22b
simonp6 <simonp1113@gmail.com>
2021-06-02 23:45:31 +0900

    modified homepage SearchView actions

--- src/Components/Views/SearchView.js
+++ src/Components/Views/SearchView.js
@@ -10,16 +10,7 @@ const SearchView = () => {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const redirectfunction = () => {
-    
-    history.push({
-      pathname:"/search",
-      state:{
-          data: '1'
-       }
-    });
-  }
-  const clickfunction = () => {
+  const clickfunctionReco = () => {
     var input = document.getElementById("input").value;
     if(input!= ""){
       dispatch(deleteFirstCity());
@@ -29,28 +20,51 @@ const SearchView = () => {
       dispatch(deleteFourthCity());
       dispatch(setRecommendedPlace(". . ."))
       SearchMap.searchMapKeyWord(input);
-      redirectfunction()
+      history.push({
+        pathname:"/search",
+        state:{
+            data: '1'
+         }
+      });
     }
   }  
   
+  const clickfunctionAdd = () => {
+    var input = document.getElementById("input").value;
+    if(input!= ""){
+      dispatch(deleteFirstCity());
+      dispatch(setFirstCity(input));
+      dispatch(deleteSecondCity());
+      dispatch(deleteThirdCity());
+      dispatch(deleteFourthCity());
+      dispatch(setRecommendedPlace(". . ."))
+      SearchMap.searchMapKeyWord(input);
+      history.push({
+        pathname:"/search",
+        state:{
+            data: '0'
+         }
+      });
+    }
+  }  
 
   var this_div = 
   <div id="mainContainer">
     <div className= 'inputWrapper' >
       <input type="text" id='input' placeholder="Add your locations" onKeyDown = {function(e){
         if(e.keyCode == 13){
-          clickfunction();
+          clickfunctionReco();
         }
       }}>
       </input>
     </div>
     <div className="btn_wrapper">
-      <div onClick={clickfunction} className="add_btn">
+      <div onClick={clickfunctionAdd} className="add_btn">
         <img  className="add_img" src={require(`../../img/Buttons/plus.png`).default} />
         {/* <div className="add_img">+</div> */}
         <div className="btnText">Add Location</div>
       </div>
-      <div onClick={clickfunction} className="recommend_btn">
+      <div onClick={clickfunctionReco} className="recommend_btn">
         <img  className="recommend_img" src={require(`../../img/Buttons/recommend.png`).default} />
         <div className="btnText">Recommend Me</div>
       </div>
